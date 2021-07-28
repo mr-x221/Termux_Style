29fcee7af52e085faa31d0e85d3cbb8801d5ea9b
luckysmile <aungh9208@gmail.com>
2021-07-28 19:03:48 +0630

    termux_style

---
+++ bash.bashrc
@@ -0,0 +1,5 @@
+
+figlet -f slant joker| lolcat
+
+PS1='\033[1;91mroot@termux[\033[1;93m\W\033[1;91m]:
+# \033[1;92m\e[0m'
---
+++ run.sh
@@ -0,0 +1,87 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+#colour
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[0;36m'
+G='\033[1;32m'
+W='\033[1;37m'
+Y='\033[1;33m'
+Q='\e[0m'
+
+#install  pickage
+clear
+apt install figlet -y
+gem install lolcat
+sleep 0.9
+clear
+
+#logo
+figlet -f bubble LUCKY SMILE| lolcat
+#echo -e "\n"
+echo "         ______________________________________         "|lolcat
+echo "________|                                      |_______ "|lolcat
+echo "\       |        WELCOME FROM LUCKY SMILE      |      / "|lolcat
+echo " \      |                                      |     /  "|lolcat
+echo " /      |______________________________________|     \  "|lolcat
+echo "/__________)                                (_________\ "|lolcat
+echo ""
+echo -e "\e[1;36m                  Coded By -> Lucky Smile\n"
+echo -e "\e[1;35m                       Version - 2.0"
+sleep 1.3
+
+echo -e "$W[ ■ □ □ □ □ □ □ □ □ □ ] 10%"
+sleep 1.1
+echo -e "[ ■ ■ □ □ □ □ □ □ □ □ ] 20%"
+sleep 1.2
+echo -e "[ ■ ■ ■ □ □ □ □ □ □ □ ] 30%"
+sleep 1.3
+echo -e "[ ■ ■ ■ ■ □ □ □ □ □ □ ] 40%"
+sleep 1.4
+echo -e "[ ■ ■ ■ ■ ■ □ □ □ □ □ ] 50%"
+sleep 1.5
+echo -e "[ ■ ■ ■ ■ ■ ■ □ □ □ □ ] 60%"
+sleep 1.6
+echo -e "[ ■ ■ ■ ■ ■ ■ ■ □ □ □ ] 70%"
+sleep 1.7
+echo -e "[ ■ ■ ■ ■ ■ ■ ■ ■ □ □ ] 80%"
+sleep 1.8
+echo -e "[ ■ ■ ■ ■ ■ ■ ■ ■ ■ □ ] 90%"
+sleep 2
+echo -e "[ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ] 100%"$Q
+sleep 2.1
+echo ""
+# yes or no
+
+read -p "Please Choose!! Will this business move forward?(1/2):" ans
+
+if [[ $ans -eq 1 ]];then
+
+#rm ../usr/etc/motd motd.dpkg-disk bash.bashrc
+#cp bash.bashrc ../usr/etc
+echo ""
+echo -e "$C Please Wait.....$Q"
+
+sleep 2.2
+
+pkg install sl
+
+sl
+sleep 1
+clear
+echo ""
+figlet -f term close termux and open again termux!!!| lolcat
+echo -e "$G              (OR) $Q"
+figlet -f term open new session| lolcat
+echo ""
+echo ""
+
+elif [[ $ans -eq 2 ]];then
+echo ""
+echo -e "\e[5;31m This Process is fail....$Q"
+echo ""
+else
+echo ""
+echo -e "$R Please answer 1 (or) 2 $Q"
+figlet -f term        Please Try again!!| lolcat
+fi
